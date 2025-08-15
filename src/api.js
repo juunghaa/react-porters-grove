2132ac64c7dd11dadf03f725678777c9b3ba9ff0
juunghaa <jun8ha@naver.com>
2025-08-15 17:17:17 +0900

    토큰 확인 vercel.json 주소 추가 및 api.js 수정

--- src/api.js
+++ src/api.js
@@ -12,7 +12,7 @@ export const login = async (email, password) => {
   });
 
   const data = await res.json();
-
+  console.log('로그인 백엔드 응답:', data);
   if (!res.ok) {
     throw new Error(data.detail || '로그인 실패');
   }
@@ -33,7 +33,7 @@ export const getMyProfile = async (token) => {
 
 // 회원가입 
 export const register = async (email, password1, password2) => {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/registration/`, {
+  const res = await fetch(`/api/auth/registration/`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -43,6 +43,8 @@ export const register = async (email, password1, password2) => {
     }),
   });
   const data = await res.json();
+  console.log('회원가입 백엔드 응답:', data);
+
   if (!res.ok) {
     // 백엔드에서 에러 메시지들이 객체로 올 수 있어서 예쁘게 정리
     const errorMessages = Object.values(data)
@@ -72,7 +74,7 @@ export const register = async (email, password1, password2) => {
 
 // api/index.js 등
 export const googleLogin = async (code) => {
-    const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/google/`, {
+    const res = await fetch(`/api/auth/google/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
