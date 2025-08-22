d59246779b7fb932237dcca6f1045a318334a130
juunghaa <jun8ha@naver.com>
2025-08-22 16:40:06 +0900

    메인페이지 분리 및 페이지 이동 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LogoutButton from './components/Auth/LogoutButton';
 import './App.css';
 import GithubGrass from './components/GithubGrass';
 import OAuthCallback from './pages/OAuthCallback';
+import MainPage from './pages/MainPage';
            
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -42,7 +43,7 @@ export default function App() {
         
         {isLoggedIn ? (
           <>
-          <h2>환영합니다앙</h2>
+          <MainPage />
           <LogoutButton onLogout={handleLogout}/>
           
           </>) : (
@@ -65,58 +66,3 @@ export default function App() {
         </div>
     );
 }
-
-// import React, { useState } from 'react';
-// import React from "react";
-// import LeftPanel from "./components/LeftPanel/LeftPanel";
-// import MainHome from "./components/MainHome/MainHome";
-// import ChooseOption from "./components/ChooseOption/ChooseOption";
-// import './App.css'; // 스타일 적용
-
-// export default function App() {
-//   const [isPanelCollapsed, setIsPanelCollapsed] = useState(false);
-//   const [currentPage, setCurrentPage] = useState("home");
-
-//   const handlePanelToggle = () => {
-//     setIsPanelCollapsed(!isPanelCollapsed);
-//   };
-
-//   const handleCreateNew = () => {
-//     setCurrentPage("chooseOption"); // '새로 만들기' 버튼 클릭 시 페이지 전환
-//   };
-
-//   // '홈' 버튼 클릭 시 페이지를 MainHome으로 변경하는 함수
-//   const handleHomeClick = () => {
-//     setCurrentPage("home");
-//   };
-
-//   const renderMainContent = () => {
-//     if (currentPage === "home") {
-//       return <MainHome />;
-//     } else if (currentPage === "chooseOption") {
-//       return <ChooseOption />;
-//     }
-//     return null;
-//   };
-
-//   return (
-//     <div className="App">
-//       <LeftPanel
-//         isCollapsed={isPanelCollapsed}
-//         onToggle={handlePanelToggle}
-//         onCreateNew={handleCreateNew}
-//         onHomeClick={handleHomeClick}
-//       />
-//       <div
-//         className="main-content"
-//         style={{
-//           marginLeft: isPanelCollapsed ? "60px" : "240px",
-//           transition: "margin-left 0.3s ease",
-//           width: `calc(100% - ${isPanelCollapsed ? "60px" : "240px"})`,
-//         }}
-//       >
-//         {renderMainContent()} {/* 조건부 렌더링 */}
-//       </div>
-//     </div>
-//   );
-// }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import LeftPanel from "./../components/LeftPanel/LeftPanel";
+import MainHome from "./../components/MainHome/MainHome";
+import ChooseOption from "./../components/ChooseOption/ChooseOption";
+import './../App.css'; // 스타일 적용
+
+export default function App() {
+  const [isPanelCollapsed, setIsPanelCollapsed] = useState(false);
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const handlePanelToggle = () => {
+    setIsPanelCollapsed(!isPanelCollapsed);
+  };
+
+  const handleCreateNew = () => {
+    setCurrentPage("chooseOption"); // '새로 만들기' 버튼 클릭 시 페이지 전환
+  };
+
+  // '홈' 버튼 클릭 시 페이지를 MainHome으로 변경하는 함수
+  const handleHomeClick = () => {
+    setCurrentPage("home");
+  };
+
+  const renderMainContent = () => {
+    if (currentPage === "home") {
+      return <MainHome />;
+    } else if (currentPage === "chooseOption") {
+      return <ChooseOption />;
+    }
+    return null;
+  };
+
+  return (
+    <div className="App">
+      <LeftPanel
+        isCollapsed={isPanelCollapsed}
+        onToggle={handlePanelToggle}
+        onCreateNew={handleCreateNew}
+        onHomeClick={handleHomeClick}
+      />
+      <div
+        className="main-content"
+        style={{
+          marginLeft: isPanelCollapsed ? "60px" : "240px",
+          transition: "margin-left 0.3s ease",
+          width: `calc(100% - ${isPanelCollapsed ? "60px" : "240px"})`,
+        }}
+      >
+        {renderMainContent()} {/* 조건부 렌더링 */}
+      </div>
+    </div>
+  );
+}
