d97309d53a504c58809919c6a35a49c87c55e2e3
juunghaa <jun8ha@naver.com>
2025-09-29 21:06:06 +0900

    소셜로그인 백엔드 연동 코드 수정 250929

--- src/components/Auth/GoogleLoginButton.jsx
+++ src/components/Auth/GoogleLoginButton.jsx
@@ -3,8 +3,20 @@ import React from 'react';
 export default function GoogleLoginButton({ disabled }) {
   const handleGoogle = () => {
     if (disabled) return;
+
+    // const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    // const redirectUri = "http://52.79.131.1/api/v1/auth/google/callback/"; 
+    // const scope = "email profile"; 
+    // const responseType = "code"; 
+
+    // // 구글 OAuth URL 직접 생성
+    // const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+
+    // window.location.href = googleAuthUrl;
+
     // 프론트에서는 백엔드 엔드포인트 호출만
-    window.location.href = "/api/auth/google/login/";
+    // window.location.href = "/api/auth/google/login/";
+    window.location.href = "http://52.79.131.1/api/v1/auth/google/callback/";
   };
 
   return (
