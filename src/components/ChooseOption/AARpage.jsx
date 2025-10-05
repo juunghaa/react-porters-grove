b906b3894fb7044783d4e10fefbeffd92dea9347
Danbi Oh <ohdanbi1102@naver.com>
2025-10-05 14:08:34 +0900

    작업내용 임시 저장

---
+++ src/components/ChooseOption/AARpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AARpage = ({ onBack }) => {
+  return (
+    <div>
+      <h1>AAR 페이지</h1>
+      <button onClick={onBack}>뒤로가기</button>
+    </div>
+  );
+};
+
+export default AARpage;
